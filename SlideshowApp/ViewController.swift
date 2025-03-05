449fd438db86bce5fa022e749cff4033de8a6219
Masatsugu Koga <m-koga@marueikogyo.jp>
2025-03-05 12:33:53 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Masatsugu Koga on 2025/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
